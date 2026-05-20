d8eaf8ec4c5941d40075e6e3811dabf9a65e4df3
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-20 19:07:23 +0100

    Añadí la clase circulo con su atributo radio, que utilizaremos para los métodos y su constructor

---
+++ src/net/salesianoslacuesta/actividad1/circulo/Circulo.java
@@ -0,0 +1,11 @@
+package net.salesianoslacuesta.actividad1.circulo;
+
+import net.salesianoslacuesta.actividad1.CalculoBidimencional;
+
+public class Circulo implements CalculoBidimencional {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+}
