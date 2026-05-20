faacc5060233ff29296bf22f71cd952992f242b1
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-20 20:09:05 +0100

    Añadí los métodos heredados de la clase vehiculo en la clase coche de combustión con lo que nos pedía el enunciado, los métodos eran arrancar, mover y parar motor

--- src/net/salesianoslacuesta/actividad2/cocheCombustion/CocheCombustion.java
+++ src/net/salesianoslacuesta/actividad2/cocheCombustion/CocheCombustion.java
@@ -1,5 +1,7 @@
 package net.salesianoslacuesta.actividad2.cocheCombustion;
 
+import net.salesianoslacuesta.actividad2.vehiculo.Vehiculo;
+
 public class CocheCombustion extends Vehiculo {
 
      private boolean motorEncendido;
@@ -29,5 +31,41 @@ public class CocheCombustion extends Vehiculo {
 
      public void setLitrosCombustible(double listrosCombustible) {
         this.listrosCombustible = listrosCombustible;
+     }
+
+     @Override
+     public void arrancar() {
+        if (listrosCombustible >= 1) {
+            listrosCombustible -= 1;
+            motorEncendido = true;
+            System.out.println("El coche arranco!. Combustible restante: "+ listrosCombustible + " litros.");
+
+        } else {
+            motorEncendido = false;
+            System.out.println("No arranca!. No hay combustible suficiente.");
+        }
+
+
+     @Override
+    public void pararMotor() {
+        motorEncendido = false;
+        System.out.println("El motor se ha detenido.");
+    }
+    
+    @Override
+    public void mover() {
+        if (!motorEncendido) {
+            System.out.println("No se puede mover ya que el motor está apagado.");
+            pararMotor();
+        } else if (listrosCombustible < 1) {
+            System.out.println(" No se puede mover ya que no queda combustible suficiente.");
+            pararMotor();
+        } else {
+            listrosCombustible -=1;
+            System.out.println("El coche se está moviendo!. Combustible restante: " + listrosCombustible + "litros.");
+        }   
+    }
+
+   
      }
 }
